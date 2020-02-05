2653feff40216a2301d8a86f5f22ffd124396566
Dave Winniford <dwinniford@gmail.com>
2020-02-05 13:49:23 +0000

    select tea menu

--- lib/adagio_tea_sale/cli.rb
+++ lib/adagio_tea_sale/cli.rb
@@ -29,7 +29,7 @@ class AdagioTeaSale::CLI
     puts "To sort tea by price, enter 'sort price'."
     puts "To sort tea by percent off, enter 'sort percent'."
     puts "To sort tea by price per cup, enter 'sort cup'."
-    puts "To add a tea to shopping cart, enter 'add cart'."
+    # puts "To add a tea to shopping cart, enter 'add cart'."  only give this option on the tea info menu.
     puts "To exit, enter 'exit'."
     input = gets.strip 
     case input
@@ -41,14 +41,35 @@ class AdagioTeaSale::CLI
         sort_by_percent
       when 'sort cup'
         sort_by_cup
-      when 'add cart'
-        add_to_cart
+      # when 'add cart'
+      #   add_to_cart
       when 'exit'
         exit 
       end 
     end 
-    
+  end 
   
+  def select_tea
+    puts "Please enter the name of a tea:"
+    # input = gets.strip
+    # t = Tea.find_by_name(input)
+    # puts "Name: #{t.name}" 
+    # puts "Rating: #{t.rating}" 
+    # puts "Quantity: #{t.quantity}" 
+    # puts "Price: #{t.price}" 
+    # puts "Price per cup: #{t.price_per_cup}"
+    # puts "Percent off: #{to.percent off}" 
+    # puts "More info: #{t.info}"
+    # puts "Caffeine: #{t.caffeine}"
+    # puts "Brewing instructions: #{t.brewing}"
+    puts "To add this tea to your shopping cart, enter 'add_cart'."
+    puts "To see main menu, enter 'menu'."
+    input = gets.strip
+    if input == "add_cart"
+      current_user.add_to_cart(t)
+    elsif input == "menu"
+      menu 
+    end 
   end 
     
 end 
