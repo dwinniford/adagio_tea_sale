7f94f50963879f139e3c1720c71e21c9f5b59852
Dave Winniford <dwinniford@gmail.com>
2020-02-05 15:15:57 +0000

    view cart menu

--- lib/adagio_tea_sale/cli.rb
+++ lib/adagio_tea_sale/cli.rb
@@ -17,9 +17,15 @@ class AdagioTeaSale::CLI
   
   def list_sales
     puts "Here are today's sales:"
-    #Tea.all.each...puts... or is this a resposibility of the tea class?
+    #print_tea(Tea.all)
     puts "1. Black tea"
     puts "2. Green tea"
+  end 
+  
+  def print_tea(tea_array)
+    tea_array.each do |t|
+      puts "Name: #{t.name}, Price: #{t.price}"
+    end 
   end 
   
   def menu 
@@ -29,6 +35,7 @@ class AdagioTeaSale::CLI
     puts "To sort tea by price, enter 'sort price'."
     puts "To sort tea by percent off, enter 'sort percent'."
     puts "To sort tea by price per cup, enter 'sort cup'."
+    puts "To view your shopping cart, enter 'view cart'."
     # puts "To add a tea to shopping cart, enter 'add cart'."  only give this option on the tea info menu.
     puts "To exit, enter 'exit'."
     input = gets.strip 
@@ -41,8 +48,8 @@ class AdagioTeaSale::CLI
         sort_by_percent
       when 'sort cup'
         sort_by_cup
-      # when 'add cart'
-      #   add_to_cart
+      when 'view cart'
+        view_cart
       when 'exit'
         exit 
       end 
@@ -66,10 +73,61 @@ class AdagioTeaSale::CLI
     puts "To see main menu, enter 'menu'."
     input = gets.strip
     if input == "add_cart"
-      current_user.add_to_cart(t)
+     # current_user.add_to_cart(t)
+      puts "Added_to_cart"
     elsif input == "menu"
       menu 
     end 
   end 
+  
+  def print_tea_info
+  end 
+  
+  def sort_by_price
+    #print_tea(Tea.sort_by_price)
+    puts "Tea sorted by price..."
+  end 
+  
+  def sort_by_percent
+   # print_tea(Tea.sort_by_percent)
+    puts "Tea sorted by percent..."
+  end 
+  
+  def sort_by_cup
+    #print_tea(Tea.sort_by_cup)
+    puts "Tea sorted by price per cup..."
+  end 
+  
+  def view_cart
+    # if current_user.cart.length == 0 
+    #   puts "Your cart is empty."
+    # else 
+    #   print_tea(current_user.cart)
+    # end 
+    puts "Items in your cart..."
+    puts "To remove an item, enter 'remove item'."
+    puts "To remove all items, enter 'remove all'."
+    puts "To select a tea for more information, enter 'select'."
+    puts "To view the main menu, enter 'menu'."
+    input = gets.strip
+    case input 
+      # when "remove item"
+      #   remove_item
+      # when "remove_all"
+      #   remove_all
+      when "select"
+        select_tea
+      when "menu"
+        menu 
+    end 
+  end 
+  
     
 end 
+
+
+
+
+
+
+
